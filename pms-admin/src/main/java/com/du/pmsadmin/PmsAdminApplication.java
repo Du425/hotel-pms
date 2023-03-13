99cffac3cd0199aeabe236215bef5a83ab6f2ffc
dengsht7 <dengsht7@yonyou.com>
2023-03-13 20:34:59 +0800

    pms-admin

---
+++ pms-admin/src/main/java/com/du/pmsadmin/PmsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.du.pmsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsAdminApplication.class, args);
+    }
+
+}
